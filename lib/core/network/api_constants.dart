4b10b7d0f09bda82083e462d0cf2b9574472a700
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-07-30 17:57:16 +0300

    ...

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,8 +1,10 @@
 class ApiConstants {
+
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String apiKey = 'a85ee98584174511a63399ee49e419ab';
   static const String nowPlayingMoviesPath = "$baseUrl/movie/now_playing?api_key=$apiKey";
   static const String popularMoviesPath = "$baseUrl/movie/popular?api_key=$apiKey";
   static const String topRatedMoviesPath = "$baseUrl/movie/top_rated?api_key=$apiKey";
-
+  static const String baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+   static  String imageUrl(String path) => '$baseImageUrl$path';
 }
